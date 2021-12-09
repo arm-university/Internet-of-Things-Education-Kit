702c29cc816ea1243af25876ee02d434957baeff
OyinkuroBenafa <81170416+OyinkuroBenafa@users.noreply.github.com>
2021-12-09 11:51:10 +0000

    Initial Commit
    Initial Commit

---
+++ DISCO-L475VG/html/js/config.js
@@ -0,0 +1,185 @@
+var index =
+{
+  "titles": {
+    "title": "Arm Education Kit",
+    "libname": "DISCO-L475VG-IOT01A Edition",
+    "libtype": "Internet of Things Education Kit"
+  },
+  "headers": [
+    {
+      "name": "Lecture Presentation",
+      "column": "modules"
+    },
+    {
+      "name": "Lab Exercises",
+      "column": "exercises"
+    }
+  ],
+  "modules": [
+    [
+      {
+        "column": "modules",
+        "name": "Module 1: Introduction to the Internet of Things",
+        "link": "Contents/Module_1/01_-_Introduction_to_the_Internet_of_Things.pptx"
+      }
+    ],
+
+    [
+      {
+        "column": "modules",
+        "name": "Module 2: IoT System Architecture and Standards",
+        "link": "Contents/Module_2/02_-_IoT_System_Architecture_and_Standards.pptx"
+      }
+    ],
+
+    [
+      {
+        "column": "modules",
+        "name": "Module 3: Introduction to Embedded Systems",
+        "link": "Contents/Module_3/03_-_Introduction_to_Embedded_Systems.pptx"
+      },
+
+    ],
+
+    [
+      {
+        "column": "modules",
+        "name": "Module 4: Hardware Platforms for IoT",
+        "link": "Contents/Module_4/04_-_Hardware_Platforms_for_IoT.pptx"
+      },
+    ],
+
+    [
+      {
+        "column": "modules",
+        "name": "Module 5: The Arm Cortex-M4 Processor Architecture",
+        "link": "Contents/Module_5/05_-_The_Arm_Cortex-M4_Processor_Architecture.pptx"
+      },
+
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 6: Interrupts and Low Power Features ",
+        "link": "Contents/Module_6/06_-_Interrupts_and_Low_Power_Features.pptx"
+      },
+    ],
+
+
+    [
+      {
+        "column": "modules",
+        "name": "Module 7: Introduction to the Mbed Platform ",
+        "link": "Contents/Module_7/07_-_Introduction_to_the_Mbed_Platform.pptx"
+      },
+      {
+        "column": "exercises",
+        "name": "Lab 1: Getting started guide",
+        "link": "Contents/Module_7/Lab_1_-_Getting_Started.docx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 8: IoT Connectivity Part 1 ",
+        "link": "Contents/Module_8/08_-_IoT_Connectivity__Part_I.pptx"
+      },
+      {
+        "column": "exercises",
+        "name": "Lab 2: IO and GPIO",
+        "link": "Contents/Module_8/Lab_2_-_IO_and_GPIO.docx"
+      }
+    ],
+
+    [
+      {
+        "column": "modules",
+        "name": "Module 9: IoT Connectivity Part 2 ",
+        "link": "Contents/Module_9/09_-_IoT_Connectivity__Part_II.pptx"
+      },
+      {
+        "column": "exercises",
+        "name": "Lab 3: Basic Mobile Application",
+        "link": "Contents/Module_9/Lab_3_-_Basic_Mobile_App.docx"
+      },
+
+      {
+        "column": "exercises",
+        "name": "Lab 4: BLE Program",
+        "link": "Contents/Module_9/Lab_4_-_BLE_Program.docx"
+      },
+    ],
+
+    [
+      {
+        "column": "modules",
+        "name": "Module 10: The Cloud ",
+        "link": "Contents/Module_10/10_-_The_Cloud.pptx"
+      },
+      {
+        "column": "exercises",
+        "name": "Lab 5: Heart Rate Android Application",
+        "link": "Contents/Module_10/Lab_5_-_Android_App__HR.docx"
+      }
+    ],
+
+    [
+      {
+        "column": "modules",
+        "name": "Module 11: IoT Security ",
+        "link": "Contents/Module_11/11_-_IoT_Security.pptx"
+      },
+      {
+        "column": "exercises",
+        "name": "Lab 6: IoT Sensors",
+        "link": "Contents/Module_11/Lab_6_-_IoT_Sensors.docx"
+      },
+      {
+        "column": "exercises",
+        "name": "Lab 7: Weather Station Application",
+        "link": "Contents/Module_11/Lab_7_-_IoT_SensorAndApp.docx"
+      },
+
+    ],
+
+    [
+
+      {
+        "column": "modules",
+        "name": "Module 12: Current and Future trends of IoT ",
+        "link": "Contents/Module_12/12_-_Current___Future_IoT_Trends.pptx"
+      },
+      {
+        "column": "exercises",
+        "name": "Lab 8: WiFi Lab",
+        "link": "Contents/Module_12/Lab_8-WiFi_Lab.docx"
+      }
+    ],
+    [
+
+      {
+        "column": "exercises",
+        "name": "Final Lab: Activity Recognition",
+        "link": "Contents/Module_13/Final_Project-_Activity_recognition.docx"
+      }
+    ],
+    [
+
+      {
+        "column": "exercises",
+        "name": "Quizzes",
+        "link": "Contents/Quizzes/Quizzes.docx"
+      },
+      {
+        "column": "exercises",
+        "name": "Exam",
+        "link": "Contents/Exam/Paper.docx"
+      },
+      {
+        "column": "exercises",
+        "name": "Exam Solution",
+        "link": "Contents/Exam/Solution.docx"
+      }
+    ]
+  ]
+}
---
+++ README.md
@@ -0,0 +1,48 @@
+
+# Internet-of-Things-Education-Kit
+
+Welcome to our Internet of Things Education Kit!
+
+### [Download the EdKit here](https://github.com/arm-university/Internet-of-Things-Education-Kit/archive/refs/heads/main.zip)
+
+Our flagship offering to universities worldwide is the Arm University Program Education Kit series.
+
+These self-contained educational materials offered exclusively and at no cost to academics and teaching staff worldwide. They’re designed to support your day-to-day teaching on core electronic engineering and computer science subjects. You have the freedom to choose which modules to teach – you can use all the modules in the Education Kit or only those that are most appropriate to your teaching outcomes.
+
+Our Internet of Things Education Kit covers the  fundamental concepts of the Internet of Things (IoT), such as embedded systems, connectivity, cloud computing, and security. The Education Kit provides student with practical experience in creating smartphone apps and controlling a wearable device using the Arm Mbed platform and Android SDK. This kit focuses on the transformative intersection between the internet, mobile and sensor technology and providing the skill set to get involved in IoT development. A full description of the education kit can be found [here](https://www.arm.com/resources/education/education-kits/internet-of-things).
+
+
+ ## Kit specification:
+
+* A full set of lecture slides, ready for use in a typical 10-12-week undergraduate course (full syllabus below).
+* Lab manuals with solutions for faculty. Labs use low cost, powerful hardware boards.
+* **Prerequisites:** Basics of programming in C/ C++, entry level Java programming.
+
+## Course Aim
+To produce students who can specify, design, and program modern connected electronic systems in response to the ever-growing number of connected devices.
+
+## Syllabus
+1. Introduction to the Internet of Things
+1. IoT System Architectures and Standards 
+1. Introduction to Embedded Systems
+1. Hardware Platforms for IoT
+1. The Arm Cortex-M4 Processor Architecture
+1. Interrupts and Low Power Features
+1. Introduction to the Mbed Platform and CMSIS
+1. IoT Connectivity Part I 
+1. IoT Connectivity Part II
+1. The Cloud
+1. IoT Security
+1. Current and Future IoT Trends
+
+## License
+You are free to fork or clone this material. See [LICENSE.md](https://github.com/arm-university/Internet-of-Things-Education-Kit/blob/main/License/LICENSE.md) for the complete license.
+
+## Inclusive Language Commitment
+Arm is committed to making the language we use inclusive, meaningful, and respectful. Our goal is to remove and replace non-inclusive language from our vocabulary to reflect our values and represent our global ecosystem.
+ 
+Arm is working actively with our partners, standards bodies, and the wider ecosystem to adopt a consistent approach to the use of inclusive language and to eradicate and replace offensive terms. We recognise that this will take time. This course may contain references to non-inclusive language; it will be updated with newer terms as those terms are agreed and ratified with the wider community. We recognise that some of you will be accustomed to using the previous terms and may not immediately recognise their replacements. Please refer to the following examples:
+
+•	When introducing the AMBA AHB Protocols, we will use the term ‘Manager’ instead of ‘Master’ and ‘Subordinate’ instead of ‘Slave’. 
+ 
+Contact us at education@arm.com with questions or comments about this course. You can also report non-inclusive and offensive terminology usage in Arm content at terms@arm.com.
