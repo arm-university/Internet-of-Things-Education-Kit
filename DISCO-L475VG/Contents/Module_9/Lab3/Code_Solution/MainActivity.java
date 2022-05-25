0a1a81fbc421f0dc74ec063b6104f5d01673e937
Mark <42671970+MarkA21@users.noreply.github.com>
2022-05-25 17:03:50 +0100

    Uploaded skeleton and solution code for labs

---
+++ DISCO-L475VG/Contents/Module_9/Lab3/Code_Solution/MainActivity.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022 Arm Limited and affiliates
+ *
+ * Please see license at: https://github.com/arm-university/Internet-of-Things-Education-Kit/blob/main/License/LICENSE.md
+ */
+
+package com.example.labtest1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello from onCreate!");
+    }
+
+    public void displayMessage(View view) {
+        Toast.makeText(MainActivity.this,"Hello from button!",Toast.LENGTH_SHORT).show();
+    }
+
+}
